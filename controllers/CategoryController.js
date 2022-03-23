9a23abc5b7660f438aa694a58bada8beb4d5cd56
Mansea <6210742034@student.tu.ac.th>
2022-03-23 17:07:24 +0700

    add Task and add Categories

--- controllers/CategoryController.js
+++ controllers/CategoryController.js
@@ -1,17 +1,39 @@
 const User = require("../model/user");
-// const Category = require("../model/Categories");
+const Category = require("../model/Categories");
 
 const AddCategory = async (req, res) => {
-  const user = await User.findOne({_id: req.params.id});
-  user.Categories.push(req.body);
-  user.save();
-  res.send(user);
+  const user = await User.findOne({ _id: req.params.id });
+  if (user) {
+    user.Categories.push(req.body);
+    user.save();
+
+    res.status(201).send(user.Categories);
+  } else {
+    res.status(400).send({ success: false, message: "User id is invalid!!" });
+  }
 };
 
+
 const addTask = async (req, res) => {
-  const user = await User.findOne({_id: req.params.id}, {Categories: {name: req.params.name}});
-  user.Categories[0].details.push(req.body)
-  user.save()
-  res.send(user)
-}
+  const user = await User.findOne({ _id: req.params.id })
+    .where("Categories.name")
+    .equals(req.params.name);
+
+  if (user) {
+    var index = 0;
+    console.log(user.Categories.length);
+    for (let i = 0; i < user.Categories.length; i++) {
+      if (user.Categories[i].name === req.params.name) {
+        index = i;
+      }
+    }
+    console.log(index);
+    
+    await user.Categories[index].details.push(req.body);
+    user.save()
+    res.send(user);
+  } else {
+    return res.send({ success: false, message: "Category Invalid!!" });
+  }
+};
 module.exports = { AddCategory, addTask };
